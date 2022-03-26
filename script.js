5ca82c94d4872980a0b06e6332b30e7ceffe9e3d
JafaryNm <jaffarymdegela@gmail.com>
2022-03-26 22:40:34 +0300

    Variable Mutation

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
+
+/*************************** *
+ 
+* Variables and Datatypes
+
 var firstName='John';
-console.log(firstName);
 var lastName='Smith';
 var age=28;
 
 var fullAge=true;
 var job;
 job='teacher';
+*/
 
-var $7jeasy='ronaldo'
-var if =2;
+// Variable naming Rules 
+ 
+
+/*************************** *
+ 
+* Variables Mutations and Type Coercion
+
+*******************************/
+var firstName='John';
+age=28;
+// Type Coercion
+var job,isMarried;
+job='Teacher';
+isMarried='False';
+
+console.log(firstName + 'have'+ age + 'years old'+ 'his job is'+job + 'is he married?'+ isMarried);
+// Variable Mutations
+
+age=35;
+job='Developer'
+
+// Using alert pop window
+
+alert(firstName + 'have'+ ' ' + age + 'years old'+ 'his job is'+job + 'is he married?'+ isMarried);
+
+// Getting Values to Users
+var lastName=prompt('What is first name');
+console.log(firstName +' '+lastName);
 
 
-console.log(lastName);
-console.log(if);
-console.log(fullAge);
-console.log(job);
-console.log($7jeasy);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
