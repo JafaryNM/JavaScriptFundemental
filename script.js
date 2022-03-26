2748cfd7b4b92a190677f752263ffbee5859ce2f
JafaryNm <jaffarymdegela@gmail.com>
2022-03-26 22:01:36 +0300

    Variable concept phase 1

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+var firstName='John';
+console.log(firstName);
+var lastName='Smith';
+var age=28;
+
+var fullAge=true;
+var job;
+job='teacher';
+
+var $7jeasy='ronaldo'
+var if =2;
+
+
+console.log(lastName);
+console.log(if);
+console.log(fullAge);
+console.log(job);
+console.log($7jeasy);
