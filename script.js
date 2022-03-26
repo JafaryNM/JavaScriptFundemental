c03deda39c051c2082fd7d4db7589cd0788fcabe
JafaryNm <jaffarymdegela@gmail.com>
2022-03-26 23:58:31 +0300

    Basic Operators

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ job='teacher';
  
 * Variables Mutations and Type Coercion
 
-*******************************/
+
 var firstName='John';
 age=28;
 // Type Coercion
@@ -40,6 +40,76 @@ alert(firstName + 'have'+ ' ' + age + 'years old'+ 'his job is'+job + 'is he mar
 // Getting Values to Users
 var lastName=prompt('What is first name');
 console.log(firstName +' '+lastName);
+*/
+
+/**
+ * Basic Operators 
+ */
+
+// Math Operators
+
+var ageJafary, ageJohn, currentYear,johnYearOfBirth,jafaryYearOfBirth;
+ ageJohn=28;
+ ageJafary=27;
+ currentYear=2022;
+ johnYearOfBirth=currentYear-ageJohn;
+ jafaryYearOfBirth=currentYear-ageJafary;
+
+console.log(johnYearOfBirth);
+console.log(jafaryYearOfBirth);
+console.log(currentYear+2);
+console.log(currentYear*2);
+console.log(currentYear/10);
+
+// Logic operators
+
+var ageOlder=ageJohn>ageJafary;
+
+
+console.log(ageOlder);
+
+// Typeof operators
+var x;
+var message='JavaScript for FrontEnd developer';
+
+console.log(typeof ageOlder);
+console.log(typeof ageJafary);
+console.log(typeof x);
+console.log(typeof message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
